833d3cb44d3ac0dbdc8d84720d0f208ce0ffd5fd
Anabella <anabellaspinelli@gmail.com>
2021-11-27 20:08:45 +0100

    Parse stdin and command args

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,53 @@
-import { sum } from './index';
+import { parseInput } from './index';
 
-describe('sum', () => {
-  it('returns the sum of the 2 numbers passed in', () => {
-    expect(sum(1, 6)).toEqual(7);
+describe('parseInput', () => {
+  it('returns an array of a single cron for a single line of input', () => {
+    expect(parseInput('30 1 /bin/run_me_daily')).toEqual([
+      {
+        command: '/bin/run_me_daily',
+        config: {
+          minutes: '30',
+          hour: '1',
+        },
+      },
+    ]);
+  });
+
+  it('returns an array crons for a multiline input', () => {
+    expect(
+      parseInput(`30 1 /bin/run_me_daily
+      45 * /bin/run_me_hourly
+      * * /bin/run_me_every_minute
+      * 19 /bin/run_me_sixty_times`)
+    ).toEqual([
+      {
+        command: '/bin/run_me_daily',
+        config: {
+          minutes: '30',
+          hour: '1',
+        },
+      },
+      {
+        command: '/bin/run_me_hourly',
+        config: {
+          minutes: '45',
+          hour: '*',
+        },
+      },
+      {
+        command: '/bin/run_me_every_minute',
+        config: {
+          minutes: '*',
+          hour: '*',
+        },
+      },
+      {
+        command: '/bin/run_me_sixty_times',
+        config: {
+          minutes: '*',
+          hour: '19',
+        },
+      },
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
-export function sum(a: number, b: number): number {
-  return a + b;
+/* cron prediction script */
+
+const stdin = process.openStdin();
+
+var data = '';
+
+stdin.on('data', function (chunk: string) {
+  data += chunk;
+});
+
+stdin.on('end', function () {
+  const crons = parseInput(data);
+  var currentTime = process.argv.slice(2)[0];
+  console.log({ crons, currentTime });
+});
+
+/* ------------------------------------ */
+/* support functions & type definitions */
+/* ------------------------------------ */
+interface CronJob {
+  command: string;
+  config: {
+    minutes: string;
+    hour: string;
+  };
 }
+
+function parseInput(input: string): CronJob[] {
+  const inputLines = input
+    .trim()
+    .split('\n')
+    .map((line) => line.trim());
+
+  return inputLines.map((line) => {
+    const [minutes, hour, command] = line.split(' ');
+
+    if (!minutes || !hour || !command) {
+      throw new Error('Invalid cron input: ' + input);
+    }
+
+    return {
+      command,
+      config: {
+        minutes,
+        hour,
+      },
+    };
+  });
+}
+
+export { parseInput };
